ef725fc5968a8313223dfafcf20ddd5098e8b70a
zzqDeco <41999232+zzqDeco@users.noreply.github.com>
2026-07-17 16:21:46 +0800

    Use shiki defaultColor:false and replace !important overrides with CSS variables (#29)

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
         light: 'github-light',
         dark: 'github-dark-dimmed',
       },
+      defaultColor: false,
       wrap: true,
     },
     remarkPlugins: [remarkGfm, remarkMath],
